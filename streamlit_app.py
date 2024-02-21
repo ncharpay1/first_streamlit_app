066fd19c781ff166fb964cacef467a757d39599f
ncharpay1 <126220125+ncharpay1@users.noreply.github.com>
2024-02-20 23:37:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect-python
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT (Ncharpa, UM01712, US-East (Ohio)")
-my_data_row = my_cur.fetchone (1.25.0)
+my_data_row = my_cur.fetchone ()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
