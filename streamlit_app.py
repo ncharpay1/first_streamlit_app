996322608279e5784aa391c5b1dbf6d25091096c
ncharpay1 <126220125+ncharpay1@users.noreply.github.com>
2024-02-22 22:13:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets['python'])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(NCHAPA), CURRENT_ACCOUNT(UM01712), CURRENT_REGION(US East (Ohio)")
+my_cur.execute("SELECT CURRENT_USER(NCHAPA), (CURRENT_ACCOUNT(UM01712), (CURRENT_REGION(US East (Ohio)")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
