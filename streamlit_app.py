db8e2fdecce1f2127013b94b42c0f6270b2b89ae
ncharpay1 <126220125+ncharpay1@users.noreply.github.com>
2024-02-22 22:08:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,14 +43,7 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from_load_list")
-my_data_row = my_cur.fetchone ()
-streamlit.text("The  fruit load list contains:")
-streamlit.text(my_data_row)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 
