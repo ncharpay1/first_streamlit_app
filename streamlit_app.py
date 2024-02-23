eb74afc7e02d9847690453e32cc7135b170289be
ncharpay1 <126220125+ncharpay1@users.noreply.github.com>
2024-02-23 13:11:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**(streamlit==1.25.secrets["snowflake_connector_python"])
-my_cur = my_cnx.cursor()
+my_cur = my_cnx.cursor(10.0)
 my_cur.execute("SELECT CURRENT_USER('ncharpa'),CURRENT_ACCOUNT(DJXRCJJ-UM01712),CURRENT_REGION(US East (Ohio)")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
