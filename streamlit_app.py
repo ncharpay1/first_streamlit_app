456442bba256c4bb950c50dbd77dfb6091b7e863
ncharpay1 <126220125+ncharpay1@users.noreply.github.com>
2024-02-22 22:11:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["python"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(NCHAPA), CURRENT_ACCOUNT(UM01712), CURRENT_REGION(US East (Ohio)")
 my_data_row = my_cur.fetchone()
