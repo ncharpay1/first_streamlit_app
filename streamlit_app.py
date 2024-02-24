d11d20f6e0abb695a90fee4a13b49aca5f69e64f
ncharpay1 <126220125+ncharpay1@users.noreply.github.com>
2024-02-23 20:42:46 -0600

    Update streamlit_app.py
    added number

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,9 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 my_cnx =snowflake-connector-python(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor(1.25.0)
+my_cur = my_cnx.cursor(1.25)
 my_cur.execute("SELECT CURRENT_USER(ncharpa), CURRENT_ACCOUNT(DJXRCJJ-UM01712), CURRENT_REGION(US East (Ohio)")
-my_data_row = my_cur.fetchone(10.0.1)
+my_data_row = my_cur.fetchone(10.0)
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
