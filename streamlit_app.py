8c53edadc1accb7e2e9dd7a708cc19168de34f04
ncharpay1 <126220125+ncharpay1@users.noreply.github.com>
2024-02-26 21:09:57 -0600

    Update streamlit_app.py
    pandas as pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -32,7 +32,7 @@ streamlit.write('The user entered ', Apple)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Apple")
 
 # take the json version of response and normalize it  
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
