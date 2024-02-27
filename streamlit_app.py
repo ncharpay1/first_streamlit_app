c9599e9371bb959e1f0ff0141c28c3928c6dcf1f
ncharpay1 <126220125+ncharpay1@users.noreply.github.com>
2024-02-26 21:17:00 -0600

    Update streamlit_app.py
    added

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,25 +26,39 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-Apple = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', Apple)
+# New section to display fruityvice api response
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  # Take the json version of the response and normalize it
+  fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+  
+# New section to display fruityvice api response  
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get informaiton.")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+# dont run anything past here while we troubleshoot
+except URLError as e:
+  streamlit.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Apple")
+# streamlit.header("View Our Fruit List - Add your Favorites!")
+# Snowflake related functions
+# def get_fruit_load_list():
+#   with my_cnx.cursor() as my_cur:
+#     my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+#     return my_cur.fetchall()
 
-# take the json version of response and normalize it  
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-#output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
-
-
+# Add a button to load the fruit
+# if streamlit.button('Get Fruit Load List'):
+#   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#   my_data_rows = get_fruit_load_list()
+#   my_cnx.close()
+#   streamlit.dataframe(my_data_rows)
 
 
 
