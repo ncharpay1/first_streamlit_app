a8ef1e52976f28fa0010dc5fe2d14d16da1247a3
ncharpay1 <126220125+ncharpay1@users.noreply.github.com>
2024-02-28 22:11:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ streamlit.header("View Our Fruit List - Add your Favorites!")
 #Snowflake Related functions
  def get_fruit_load_list():
    # with my_cnx.cursor() as my_cur:
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+#my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 Return my_cur.fetchall()
 
 Add a button to load the fruit
