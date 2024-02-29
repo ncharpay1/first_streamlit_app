8c69379d98fbfd9ada256e620601cdd8ca203506
ncharpay1 <126220125+ncharpay1@users.noreply.github.com>
2024-02-28 21:17:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,13 +17,13 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# New section to display fruityvice api response
+ New section to display fruityvice api response
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  # Take the json version of the response and normalize it
+   Take the json version of the response and normalize it
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
-  
+
 New section to display fruityvice api response  
 streamlit.header("Fruityvice Fruit Advice!")
 try:
