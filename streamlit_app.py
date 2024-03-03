2fe2f0beaa5085d8149ec27d7ed05b1fd1b2bc8d
ncharpay1 <126220125+ncharpay1@users.noreply.github.com>
2024-03-02 18:24:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,36 +17,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# New section to display fruityvice api response
-def get_fruityvice_data(this_fruit_choice):
- fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-# Take the json version of the response and normalize it
- fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
- return fruityvice_normalized
-
-#New section to display fruityvice api response  
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get informaiton.")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
- # dont run anything past here while we troubleshoot
-except URLError as e:
-  streamlit.error()
- 
-streamlit.header("View Our Fruit List - Add your Favorites!")
-#Snowflake Related functions
-# def get_fruit_load_list():
-   # with my_cnx.cursor() as my_cur:
-#my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-# Return my_cur.fetchall()
-
- Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
- 
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
    my_cnx.close()
